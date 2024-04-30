16a81eed7bc246a468e01a38f9ac06cb69bd3a2b
Mcayear <mcayear@163.com>
2024-04-30 10:09:47 +0800

    add: custom item example

---
+++ src/main/java/cn/nukkitmot/exampleplugin/custom/item/CandyCaneSword.java
@@ -0,0 +1,42 @@
+package cn.nukkitmot.exampleplugin.custom.item;
+
+import cn.nukkit.item.customitem.CustomItemDefinition;
+import cn.nukkit.item.customitem.ItemCustom;
+import cn.nukkit.item.customitem.data.ItemCreativeCategory;
+import cn.nukkit.item.customitem.data.RenderOffsets;
+
+public class CandyCaneSword extends ItemCustom {
+    private static String stringId = "candy_cane_sword";
+    public static String name = "糖果剑";
+
+    @Override
+    public int getMaxStackSize() {
+        return 1;
+    }
+
+    public int scaleOffset() {
+        return 32; // 需要是16的倍数，如 32、64、128
+    }
+
+    /**
+     * 该方法设置自定义物品的定义
+     */
+    @Override
+    public CustomItemDefinition getDefinition() {
+        return CustomItemDefinition
+                .simpleBuilder(this, ItemCreativeCategory.EQUIPMENT)
+                .creativeGroup("itemGroup.name.item")
+                .allowOffHand(false)
+                .renderOffsets(RenderOffsets.scaleOffset(scaleOffset()))
+                .build();
+    }
+
+    public CandyCaneSword() {
+        super(stringId, name);
+    }
+
+    @Override
+    public int getMaxDurability() {
+        return 500;
+    }
+}
